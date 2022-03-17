aa5da05050861bd151f90ff402cbcfc101c00ae2
Jerry <Jvictor940@gmail.com>
2022-03-16 22:30:01 -0400

    commit tree ffcd4271b6e33066a401080ffefb3ae78f425fb5

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const ride = 199;
+
+let scuberMessage;
+let scuberMessage2;
+
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    scuberMessage = 'a free sample'; 
+  } else {
+  scuberMessage2 = "This one is on me!";
+  }
 }
+scuberGreetingForFeet()
 
 function ternaryCheckCity(){
   // Write your code here!
